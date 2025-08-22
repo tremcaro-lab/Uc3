8d0fac6474ee589252bf3c90dc5787fd9f2fac61
luiz <gregorioluiz215@gmail.com>
2025-08-21 21:43:31 -0300

    aula 02

---
+++ aulas/lista.py
@@ -0,0 +1,19 @@
+minha_lista=[1,4,7]
+print(minha_lista)
+print(minha_lista[2])
+print(minha_lista[1:2])
+
+outra_lista=["casa",5,"e",[1,2,3]]
+
+print(outra_lista[3] [1])
+print(outra_lista[3], outra_lista[1])
+#adiciona elementos na lista
+outra_lista.append(8)
+print(outra_lista)
+#ordem crecente
+ordem=["d","A", 'C']
+ordem.sort
+print(ordem)
+#ordem decrescente
+ordem.reverse
+print(ordem)
---
+++ aulas/string.py
@@ -0,0 +1,12 @@
+#tamnho de umas string
+string="luiz Carlos"
+print(len(string))
+#indexação de strings
+print(string[0])
+print(string[0:2])
+#de trás para frente
+print(string[-1])
+#multiplicando strings
+print('x'*10)
+#criando uma split por separação
+print(string.split())
