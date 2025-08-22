4ec0f6783bb25351860e0a2cf142bcb80db00ae6
luiz <gregorioluiz215@gmail.com>
2025-08-21 21:43:14 -0300

    aula 02

---
+++ aulas/dicionário.py
@@ -0,0 +1,12 @@
+meu_dicionário ={"nome":"camila"}
+print(meu_dicionário)
+meu_dicionário2 ={"nome":"camila","idade":29}
+print(meu_dicionário["nome"])
+
+#inclusão de novos elementos
+meu_dicionário2["cidade"]= "Rio de Janeiro"
+
+#retorna uma lista com todas as chave
+meu_dicionário2.keys()
+#retorna os valores de uma chave
+meu_dicionário2.values()
