fa88d40a8f879a7e7a8bac396cbddb4bb02b541b
luiz <gregorioluiz215@gmail.com>
2025-09-18 21:31:17 -0300

    django-aula-1

--- aulas/jogando_certo.py
+++ aulas/jogando_certo.py
@@ -1,35 +1,41 @@
 from velha_certo import Velha
 
 jogo = Velha()
+# Desenha o tabuleiro inicial
+jogo.desenharTabuleiro
+while jogo.verificarEspaço():
+    while True:
+        try:
+        # Jogada do humano
+            jogadaLinha = int(input('Digite a linha: '))
+            jogadaColuna = int(input('Digite a coluna: '))
 
-while jogo.verificarEspaço ():
- # Desenha o tabuleiro inicial
- jogo.desenharTabuleiro()
- while True:
-# Jogada do humano
-  jogadaLinha = int(input('Digite a linha: '))
-  jogadaColuna = int(input('Digite a coluna: '))
-  if jogo.tabuleiro[jogadaLinha][jogadaColuna] == ' ':
-     jogo.tabuleiro[jogadaLinha][jogadaColuna] = 'X'
-     break
-jogo.desenharTabuleiro()
+            if jogo.tabuleiro[jogadaLinha][jogadaColuna] == ' ':
+             jogo.tabuleiro[jogadaLinha][jogadaColuna] = 'X'
+            break
+        except(ValueError,IndexError,):
+           print('O número não existe')
+    resultado = jogo.verificarVencedor()
+    if resultado['X']:
+      print('x venceu')
+      break  
+    if not jogo.verificarEspaço:
+        print('jogo empatou')
+        break
+    jogo.desenharTabuleiro()
 
-# Jogada da máquina
-while True:
-   jogadaLinha = jogo.jogarMaquina()
-   jogadaColuna = jogo.jogarMaquina()
-   if jogo.tabuleiro[jogadaLinha][jogadaColuna] ==' ':
-      jogo.tabuleiro[jogadaLinha][jogadaColuna] = 'O'
-      print('Máquina jogou')
-      break
-jogo.desenharTabuleiro()
+    while True:
+        # Jogada da máquina
+        jogadaLinha = jogo.jogarMaquina()
+        jogadaColuna = jogo.jogarMaquina()
 
-
-resultado=jogo.verificarVencedor()
-
-if resultado['X']:
-    print('X venceu')
-elif resultado['O']:
-    print('O venceu')
-else:
-    print('Empate')
+        if jogo.tabuleiro[jogadaLinha][jogadaColuna] == ' ':
+            jogo.tabuleiro[jogadaLinha][jogadaColuna] = 'O'
+            print('Máquina jogou')
+        break
+    if resultado['O']:
+      print('O venceu')
+      break  
+    if not jogo.verificarEspaço:
+        print('jogo empatou')
+        break
