dbcf35bee0591a24c3644587ace49732b28bbba5
luiz <gregorioluiz215@gmail.com>
2025-09-30 20:57:14 -0300

    aula12

--- aulas/aula-10-django1/hello/settings.py
+++ aulas/aula-10-django1/hello/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+#my aplicattions
+INSTALLED_APPS+=[
+    'index',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
